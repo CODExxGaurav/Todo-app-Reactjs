b13b1a98f8b904460990eb8681aeab57848f66f4
CODExxGaurav <89847886+CODExxGaurav@users.noreply.github.com>
2023-04-05 23:57:22 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./App.css";
+import TodoList from './components/TodoList';
+const App = () => {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
